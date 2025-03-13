22ee1b87316ed147a98f2214c16e91f9452545dc
Ayanyiola Enoch <ayanyiolaenoch155@gmail.com>
2025-03-14 00:44:34 +0100

    increasing text size and adding hierachy

--- src/App.jsx
+++ src/App.jsx
@@ -34,7 +34,7 @@ function App() {
       {/* LOGO */}
       <div className='p-5 font-bold border-b-2 text-white flex justify-between h-16.5 bg-gradient-to-r from-purple-500 to-pink-500 items-center fixed top-0 w-full z-50'>
         <img src='/food-png.png' alt='logo' className='h-10' />
-        <Link to="/welcome" className='hover:border-b-2 cursor-pointer text-white'>G&G Restaurant</Link>
+        <p className='hover:border-b-2 cursor-pointer text-white'>G&G Restaurant</p>
       </div>
 
       {/* MAIN CONTENT */}
@@ -43,10 +43,10 @@ function App() {
 
         <div className='relative flex flex-col md:flex-row justify-center items-center gap-6 p-5 w-full max-w-4xl'>
           <div className='p-8 shadow-xl min-h-[200px]  bg-white rounded-lg relative z-10 flex-1 flex flex-col justify-center'>
-            <div className={`px-2 py-1 text-center rounded-full text-sm font-medium ${currentStatus.isOpen ? "bg-green-100 text-green-800" : "bg-red-100 text-red-800"}`}>
+            <div className={`px-2 py-1 text-center rounded-full text-2xl font-medium ${currentStatus.isOpen ? "bg-green-100 text-green-800" : "bg-red-100 text-red-800"}`}>
               {currentStatus.isOpen ? "Open Now" : "Closed Now"}
             </div>
-            <p className="mt-2 text-center">{currentStatus.message}</p>
+            <p className="mt-2 text-center text-sm">{currentStatus.message}</p>
           </div>
 
           <div className='p-8 shadow-xl bg-white rounded-lg relative z-10 flex-1 flex flex-col justify-center'>
@@ -62,8 +62,8 @@ function App() {
         </div>
 
         {/* FOOTER */}
-        <div className='absolute bottom-0 w-full bg-white p-6 text-center shadow-xl rounded-lg'>
-          <p>
+        <div className=' absolute bottom-0 w-full bg-white p-6 text-center shadow-xl rounded-lg'>
+          <p className='text-sm mt-0.7'>
             We alternate our Sunday openings. We're {isOpen ? "open" : "closed"} on {formatDate(nextSunday)},
             and we'll be {!isOpen ? "open" : "closed"} the following Sunday.
           </p>
@@ -80,14 +80,6 @@ function App() {
 export const revalidate = 86400
 
 export default App;
-
-
-
-
-
-
-
-// import './index.css'
 // import { Route, Link, Routes } from 'react-router-dom';
 // import { isOpenThisSunday, getNextSundayDate, formatDate } from "../src/lib/utils"
 
